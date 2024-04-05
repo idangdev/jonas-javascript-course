6f828a0e0f26cf2311dcf97daee1b235a2f11ec0
idangdev <idang.dev@gmail.com>
2024-04-05 15:31:21 +0700

    10. Practice Assignment

---
+++ 01-Fundamentals-Part-1/assignments.js
@@ -0,0 +1,7 @@
+let country = "Indonesia";
+let continent = "Asia";
+let population = "279.072.446";
+
+console.log("Country :" + country);
+console.log("Continent :" + continent);
+console.log("Population :" + population);
