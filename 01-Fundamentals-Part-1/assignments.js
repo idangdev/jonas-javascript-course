57af23cebdcf5911e918baaf366d1c41e44e40ac
idangdev <idang.dev@gmail.com>
2024-04-06 00:05:48 +0700

    11. Data Types

--- 01-Fundamentals-Part-1/assignments.js
+++ 01-Fundamentals-Part-1/assignments.js
@@ -1,7 +1,20 @@
+// ###########################
+// ### Values and Variables
+
 let country = "Indonesia";
 let continent = "Asia";
 let population = "279.072.446";
 
-console.log("Country :" + country);
-console.log("Continent :" + continent);
-console.log("Population :" + population);
+// console.log("Country :" + country);
+// console.log("Continent :" + continent);
+// console.log("Population :" + population);
+
+// ###########################
+// ### Data Types
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -10,11 +11,38 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
+// Variable name conventions
+let jonas_matilda = "JM";
+let $function = 27;
+
 let person = "jonas";
 let PI = 3.1415;
 
-let myFirstJob = "Programmer";
+let myFirstJob = "Coder";
 let myCurrentJob = "Teacher";
 
 let job1 = "programmer";
-console.log("teacher");
+let job2 = "teacher";
+
+console.log(myFirstJob);
+*/
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Jonas");
+
+javascriptIsFun = "YES";
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
