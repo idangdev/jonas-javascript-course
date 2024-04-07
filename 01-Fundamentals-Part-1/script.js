8dfb39b48ec42c794a98491f6022e11cf51fa6e5
idangdev <idang.dev@gmail.com>
2024-04-08 01:04:18 +0700

    19. CHALLENGE #2: Video Solution

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -136,7 +136,6 @@ lines"
 console.log(`String
 multiple
 lines`);
-*/
 
 const age = 15;
 
@@ -157,3 +156,21 @@ if (birthYear <= 2000) {
 }
 
 console.log(century);
+*/
+
+const massMark = 78;
+const heightMark = 1.69;
+const massJohn = 92;
+const heightJohn = 1.95;
+
+const BMIMark = massMark / heightMark ** 2;
+const BMIJohn = massJohn / (heightJohn * heightJohn);
+console.log(BMIMark, BMIJohn);
+
+/* Write your code below. Good luck! 🙂 */
+
+if (BMIMark > BMIJohn) {
+  console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn})!`);
+} else {
+  console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark})!`);
+}
