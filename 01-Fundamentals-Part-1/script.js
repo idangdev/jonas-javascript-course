a82106378216055b64c0f9deeae31d3d2522a3ce
idangdev <idang.dev@gmail.com>
2024-04-08 16:26:39 +0700

    22. Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1/script.js
+++ 01-Fundamentals-Part-1/script.js
@@ -190,7 +190,6 @@ console.log("23" / "2");
 let n = "1" + 1; // '11'
 n = n - 1;
 console.log(n);
-*/
 
 // 5 falsy values: 0, ', undefined, null, NaN
 
@@ -212,4 +211,28 @@ if (height) {
   console.log("YAY! Height is defined");
 } else {
   console.log("Height is UNDEFINED");
+}
+*/
+
+const age = "18";
+if (age === 18) console.log("You just became an asult :D (strict)");
+
+if (age == 18) console.log("You just became an asult :D (loose)");
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+if (favourite === 23) {
+  console.log("Cool! 23 is an amazing number!");
+} else if (favourite === 7) {
+  console.log("7 is also a cool number");
+} else if (favourite === 9) {
+  console.log("9 is also a cool number");
+} else {
+  console.log("Number is not 23 or 7 or 9");
+}
+
+if (favourite !== 23) {
+  console.log("Why not 23?");
 }
