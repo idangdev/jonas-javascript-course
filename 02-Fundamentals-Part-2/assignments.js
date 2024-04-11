bb2d9e488b6c1d4960cbc51914a60fa47538855a
idangdev <idang.dev@gmail.com>
2024-04-11 18:07:26 +0700

    33. Functions

---
+++ 02-Fundamentals-Part-2/assignments.js
@@ -0,0 +1,12 @@
+// ###############################
+// ### Functions
+
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} million people and its capital city is ${capitalCity}`;
+}
+
+const descPortugal = describeCountry("Portugal", 10, "Lisbon");
+const descGermany = describeCountry("Germany", 83, "Berlin");
+const descFinland = describeCountry("Finland", 6, "Helsinki");
+
+console.log(descPortugal, descGermany, descFinland);
