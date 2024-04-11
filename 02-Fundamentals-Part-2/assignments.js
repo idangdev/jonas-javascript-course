71d17d8fa11f0b5778cde8bd572712673caf5aa4
idangdev <idang.dev@gmail.com>
2024-04-12 01:41:26 +0700

    35. Arrow Functions

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -17,7 +17,7 @@ console.log(descPortugal, descGermany, descFinland);
 
 // ###############################
 // ### Function Declaration vs. Expressions
-
+/*
 function percentageOfWorld1(population) {
   return (population / 7900) * 100;
 }
@@ -31,3 +31,16 @@ const percChina1 = percentageOfWorld1(1441);
 const percUSA1 = percentageOfWorld1(332);
 
 console.log(percPortugal1, percChina1, percUSA1);
+
+*/
+
+// ###############################
+// ### Arrow Functions
+
+const percentageOfWorld3 = (population) => (population / 7900) * 100;
+
+const percPortugal3 = percentageOfWorld3(10);
+const percChina3 = percentageOfWorld3(1441);
+const percUSA3 = percentageOfWorld3(332);
+
+console.log(percPortugal3, percChina3, percUSA3);
