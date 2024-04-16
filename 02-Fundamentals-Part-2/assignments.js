39efe5165eb643bbb3c1b6e4a5ccdc2a6479f156
idangdev <idang.dev@gmail.com>
2024-04-16 16:22:09 +0700

    43. Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/assignments.js
+++ 02-Fundamentals-Part-2/assignments.js
@@ -101,6 +101,18 @@ console.log(neighbours);
 
 // ###############################
 // ### Introduction to Objects
+/*
+const myCountry = {
+  country: "Finland",
+  capital: "Helsinki",
+  language: "finnish",
+  population: 6,
+  neighbours: ["Norway", "Sweden", "Russia"],
+};
+*/
+
+// ###############################
+// ### Dot vs. Bracket Notation
 
 const myCountry = {
   country: "Finland",
@@ -109,3 +121,11 @@ const myCountry = {
   population: 6,
   neighbours: ["Norway", "Sweden", "Russia"],
 };
+
+console.log(`${myCountry.country} has ${myCountry.population} million ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}.`);
+
+myCountry.population += 2;
+console.log(myCountry.population);
+
+myCountry["population"] -= 2;
+console.log(myCountry.population);
