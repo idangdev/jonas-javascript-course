1ffdc5521233f329065e4fd12c418968665a43ef
idangdev <idang.dev@gmail.com>
2024-04-17 02:26:13 +0700

    45. CHALLENGE #3: Video Solution

--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -299,6 +299,8 @@ console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his bes
 // ###############################
 // ### Object Methods
 
+/*
+
 const jonas = {
   firstName: "Jonas",
   lastName: "Schmedtmann",
@@ -335,3 +337,42 @@ console.log(jonas.age);
 // Challenge
 // "Jonas is a 46-year old teacher, and he has a/no driver's license"
 console.log(jonas.getSummary());
+*/
+
+// ###############################
+// ### CHALLENGE #3: Video Solution
+
+/* Write your code below. Good luck! 🙂 */
+
+const mark = {
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+const john = {
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+
+  calcBMI: function () {
+    this.bmi = this.mass / this.height ** 2;
+    return this.bmi;
+  },
+};
+
+mark.calcBMI();
+john.calcBMI();
+console.log(mark.bmi, john.bmi);
+
+// "John Smith's BMI (28.3) is higher than Mark Miller's (23.9)!"
+
+if (mark.bmi > john.bmi) {
+  console.log(`${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})!`);
+} else if (john.bmi > mark.bmi) {
+  console.log(`${john.fullName}'s BMI (${john.bmi}) is higher than ${mark.fullName}'s (${mark.bmi})!`);
+}
