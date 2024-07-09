faeb1588a636b77612c8a107ef50e7ee9def865e
idangdev <idang.dev@gmail.com>
2024-07-09 20:37:05 +0700

    121. Coding Challenge #3

--- 09-Data-Structures-Operators/challenges.js
+++ 09-Data-Structures-Operators/challenges.js
@@ -120,6 +120,8 @@ BONUS: Create an object called 'scorers' which contains the names of the players
 GOOD LUCK 😀
 */
 
+/*
+
 const game = {
   team1: 'Bayern Munich',
   team2: 'Borrussia Dortmund',
@@ -161,24 +163,9 @@ const game = {
   },
 };
 
-/*
-
-1. Loop over the game.scored array and print each player name to the console, along with the goal number (Example: "Goal 1: Lewandowski")
-
-1. Loop array game.scored dan cetak setiap nama pemain ke console, beserta nomor goalnya (Contoh: "Sasaran 1: Lewandowski")
-
-*/
-
 for (const [i, player] of game.scored.entries())
   console.log(`Goal ${i + 1}: ${player}`);
 
-/*
-
-2. Use a loop to calculate the average odd and log it to the console (We already studied how to calculate averages, you can go check if you don't remember)
-
-2. Gunakan loop untuk menghitung rata-rata odd dan mencatatnya ke console (Kami telah mempelajari cara menghitung rata-rata, Anda dapat memeriksanya jika Anda tidak ingat)
-
-*/
 
 const odds = Object.values(game.odds);
 let average = 0;
@@ -190,49 +177,10 @@ for (const odd of odds) {
 average /= odds.length;
 console.log(average);
 
-/*
-
-3. Print the 3 odds to the console, but in a nice formatted way, exaclty like this:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
-Get the team names directly from the game object, don't hardcode them (except for "draw"). HINT: Note how the odds and the game objects have the same property names 😉
-
-
-3. Cetak 3 odds ke console, tetapi dengan format yang bagus, persis seperti ini:
-      Odd of victory Bayern Munich: 1.33
-      Odd of draw: 3.25
-      Odd of victory Borrussia Dortmund: 6.5
-Dapatkan nama tim langsung dari objek game, jangan melakukan hardcode (kecuali untuk "draw"). PETUNJUK: Perhatikan bagaimana odd dan objek game memiliki nama properti yang sama 😉
-
-*/
-
 for (const [team, odd] of Object.entries(game.odds)) {
   const teamStr = team === 'x' ? 'draw' : `victory ${game[team]}`;
   console.log(`Odd of ${teamStr}: ${odd}`);
 }
-
-/*
-
-BONUS: Create an object called 'scorers' which contains the names of the players who scored as properties, and the number of goals as the value. In this game, it will look like this:
-      {
-        Gnarby: 1,
-        Hummels: 1,
-        Lewandowski: 2
-      }
-
-GOOD LUCK 😀
-
-BONUS: Buat objek bernama 'scorers' yang berisi nama pemain yang mencetak gol sebagai properti, dan jumlah gol sebagai nilainya. Dalam game ini tampilannya akan seperti ini:
- {
- Gnarby: 1,
- Hummel: 1,
- Lewandowski: 2
- }
-
-SELAMAT BERUNTUNG 😀
-
-*/
 
 let scorers = {};
 
@@ -241,3 +189,90 @@ for (const player of game.scored) {
 }
 
 console.log(scorers);
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #3
+
+/* 
+Let's continue with our football betting app! This time, we have a map with a log of the events that happened during the game. The values are the events themselves, and the keys are the minutes in which each event happened (a football game has 90 minutes plus some extra time).
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+GOOD LUCK 😀
+*/
+
+const gameEvents = new Map([
+  [17, '⚽️ GOAL'],
+  [36, '🔁 Substitution'],
+  [47, '⚽️ GOAL'],
+  [61, '🔁 Substitution'],
+  [64, '🔶 Yellow card'],
+  [69, '🔴 Red card'],
+  [70, '🔁 Substitution'],
+  [72, '🔁 Substitution'],
+  [76, '⚽️ GOAL'],
+  [80, '⚽️ GOAL'],
+  [92, '🔶 Yellow card'],
+]);
+
+/*
+
+1. Create an array 'events' of the different game events that happened (no duplicates)
+1. Buat array 'events' dari berbagai peristiwa permainan yang terjadi (tidak ada duplikat)
+
+*/
+
+const events = [...new Set(gameEvents.values())];
+
+console.log(events);
+
+/* 
+
+2. After the game has finished, is was found that the yellow card from minute 64 was unfair. So remove this event from the game events log.
+
+2. Setelah pertandingan selesai, diketahui bahwa kartu kuning sejak menit ke 64 tidak adil. Jadi hapus acara ini dari log peristiwa game.
+
+*/
+
+gameEvents.delete(64);
+console.log(gameEvents);
+
+/*
+
+3. Print the following string to the console: "An event happened, on average, every 9 minutes" (keep in mind that a game has 90 minutes)
+
+3. Cetak string berikut ke konsol: "Suatu peristiwa terjadi, rata-rata, setiap 9 menit" (perlu diingat bahwa permainan memiliki waktu 90 menit)
+
+*/
+
+console.log(
+  `An event happened, on average, every ${90 / gameEvents.size} minutes`
+);
+
+const time = [...gameEvents.keys()].pop();
+console.log(time);
+
+console.log(
+  `An event happened, on average, every ${time / gameEvents.size} minutes`
+);
+
+/*
+
+4. Loop over the events and log them to the console, marking whether it's in the first half or second half (after 45 min) of the game, like this:
+      [FIRST HALF] 17: ⚽️ GOAL
+
+4. Ulangi kejadian tersebut dan catat ke konsol, tandai apakah itu terjadi di babak pertama atau kedua (setelah 45 menit) permainan, seperti ini:
+ [BABAK PERTAMA] 17: ⚽️ GOL
+
+*/
+
+for (const [min, event] of gameEvents.entries()) {
+  const half = min <= 45 ? 'FIRST' : 'SECOND';
+  console.log(`[${half} HALF] ${min}: ${event}`);
+}
