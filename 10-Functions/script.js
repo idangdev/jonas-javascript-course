49181f94a04057c91c7dacb7e4ea3526d3756291
idangdev <idang.dev@gmail.com>
2024-08-11 00:53:13 +0700

    132. Functions Accepting Callback Functions

--- 10-Functions/script.js
+++ 10-Functions/script.js
@@ -35,6 +35,7 @@ createBooking('LH123', undefined, 1000);
 ////////////////////////////////////
 // How Passing Arguments Workd: Value vs. Reference
 
+/*
 const flight = 'LH234';
 const jonas = {
   name: 'Jonas Schmedtmann',
@@ -66,3 +67,35 @@ const newPassport = function (person) {
 
 newPassport(jonas);
 checkIn(flight, jonas);
+
+*/
+
+////////////////////////////////////
+// Functions Accepting Callback Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+// Higher-order function
+const transformer = function (str, fn) {
+  console.log(`Original string: ${str}`);
+  console.log(`Transformed string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best!', upperFirstWord);
+transformer('JavaScript is the best!', oneWord);
+
+// JS uses callback all the time
+const high5 = function () {
+  console.log('👋');
+};
+document.body.addEventListener('click', high5);
+['Jonas', 'Martha', 'Adam'].forEach(high5);
