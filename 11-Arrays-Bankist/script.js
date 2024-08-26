5e03ffc7aa1faca0e2b8dcafbe46486526ced7f2
idangdev <idang.dev@gmail.com>
2024-08-27 01:14:01 +0700

    168. Coding Challenge #4

--- 11-Arrays-Bankist/script.js
+++ 11-Arrays-Bankist/script.js
@@ -687,6 +687,7 @@ labelBalance.addEventListener('click', function () {
 
 ///////////////////////////////////////
 // Array Methods Practice
+/*
 
 // 1.
 // const bankDepositSum = accounts
@@ -762,3 +763,143 @@ const convertTitleCase = function (title) {
 console.log(convertTitleCase('this is a nice title'));
 console.log(convertTitleCase('this is a LONG title but not too long'));
 console.log(convertTitleCase('and here is another title with an EXAMPLE'));
+
+*/
+
+///////////////////////////////////////
+// Coding Challenge #4
+
+/* 
+Julia and Kate are still studying dogs, and this time they are studying if dogs are eating too much or too little.
+Eating too much means the dog's current food portion is larger than the recommended portion, and eating too little is the opposite.
+Eating an okay amount means the dog's current food portion is within a range 10% above and 10% below the recommended portion (see hint).
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+HINT 1: Use many different tools to solve these challenges, you can use the summary lecture to choose between them 😉
+HINT 2: Being within a range 10% above and below the recommended portion means: current > (recommended * 0.90) && current < (recommended * 1.10). Basically, the current portion should be between 90% and 110% of the recommended portion.
+
+TEST DATA:
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] }
+];
+
+GOOD LUCK 😀
+*/
+
+/*
+
+1. Loop over the array containing dog objects, and for each dog, calculate the recommended food portion and add it to the object as a new property. Do NOT create a new array, simply loop over the array. Forumla: recommendedFood = weight ** 0.75 * 28. (The result is in grams of food, and the weight needs to be in kg)
+
+1. Lakukan loop pada array yang berisi objek dogs, dan untuk setiap anjing, hitung porsi makanan yang direkomendasikan dan tambahkan ke objek sebagai properti baru. JANGAN membuat array baru, cukup lakukan pengulangan pada array tersebut. Forumla: recommendedFood = weight ** 0,75 * 28. (Hasilnya dalam gram makanan, dan beratnya harus dalam kg)
+
+*/
+
+const dogs = [
+  { weight: 22, curFood: 250, owners: ['Alice', 'Bob'] },
+  { weight: 8, curFood: 200, owners: ['Matilda'] },
+  { weight: 13, curFood: 275, owners: ['Sarah', 'John'] },
+  { weight: 32, curFood: 340, owners: ['Michael'] },
+];
+
+dogs.forEach(dog => {
+  dog.recFood = Math.trunc(dog.weight ** 0.75 * 28);
+});
+
+console.log(dogs);
+
+/*
+
+2. Find Sarah's dog and log to the console whether it's eating too much or too little. HINT: Some dogs have multiple owners, so you first need to find Sarah in the owners array, and so this one is a bit tricky (on purpose) 🤓
+
+2. Temukan anjing Sarah dan masuk ke konsol untuk mengetahui apakah anjing tersebut makan terlalu banyak atau terlalu sedikit. PETUNJUK: Beberapa anjing memiliki banyak pemilik, jadi Anda harus terlebih dahulu menemukan Sarah di deretan pemilik, jadi yang ini agak sulit (sengaja) 🤓
+*/
+
+const dogSarah = dogs.find(dog => dog.owners.includes('Sarah'));
+
+console.log(dogSarah);
+
+console.log(
+  `Sarah's dog is eating too ${
+    dogSarah.curFood > dogSarah.recFood ? 'much' : 'little'
+  }`
+);
+
+/*
+
+3. Create an array containing all owners of dogs who eat too much ('ownersEatTooMuch') and an array with all owners of dogs who eat too little ('ownersEatTooLittle').
+
+3. Buat array yang berisi semua pemilik anjing yang makan terlalu banyak ('ownersEatTooMuch') dan array dengan semua pemilik anjing yang makan terlalu sedikit ('ownersEatTooLittle').
+
+*/
+
+const ownersEatTooMuch = dogs
+  .filter(dog => dog.curFood > dog.recFood)
+  .flatMap(dog => dog.owners);
+const ownersEatTooLittle = dogs
+  .filter(dog => dog.curFood < dog.recFood)
+  .flatMap(dog => dog.owners);
+
+console.log(ownersEatTooMuch);
+console.log(ownersEatTooLittle);
+
+/*
+
+4. Log a string to the console for each array created in 3., like this: "Matilda and Alice and Bob's dogs eat too much!" and "Sarah and John and Michael's dogs eat too little!"
+
+4. Catat string ke konsol untuk setiap array yang dibuat di 3., seperti ini: "Anjing Matilda, Alice, dan Bob makan terlalu banyak!" dan "Anjing Sarah, John, dan Michael makan terlalu sedikit!"
+
+*/
+
+console.log(`${ownersEatTooMuch.join(' and ')}'s dogs eat too much!`);
+console.log(`${ownersEatTooLittle.join(' and ')}'s dogs eat too little!`);
+
+/* 
+
+5. Log to the console whether there is any dog eating EXACTLY the amount of food that is recommended (just true or false)
+
+5. Masuk ke konsol apakah ada anjing yang memakan makanan dalam jumlah yang direkomendasikan (hanya benar atau salah)
+
+*/
+
+console.log(dogs.some(dog => dog.curFood === dog.recFood));
+
+/*
+
+6. Log to the console whether there is any dog eating an OKAY amount of food (just true or false)
+
+6. Masuk ke konsol apakah ada anjing yang memakan makanan dalam jumlah yang BENAR (hanya benar atau salah)
+*/
+const checkEatingOkay = dog =>
+  dog.curFood > dog.recFood * 0.9 && dog.curFood < dog.recFood * 1.1;
+
+console.log(dogs.some(checkEatingOkay));
+
+/*
+
+7. Create an array containing the dogs that are eating an OKAY amount of food (try to reuse the condition used in 6.)
+
+7. Buat array yang berisi anjing-anjing yang memakan makanan dalam jumlah yang BENAR (coba gunakan kembali kondisi yang digunakan pada nomor 6.)
+*/
+
+const dogsOKAY = dogs.filter(checkEatingOkay);
+console.log(dogsOKAY);
+
+/*
+8. Create a shallow copy of the dogs array and sort it by recommended food portion in an ascending order (keep in mind that the portions are inside the array's objects)
+
+8. Buat salinan dangkal dari array dogs dan urutkan berdasarkan recommended food portion dalam urutan ascending (ingatlah bahwa porsinya ada di dalam objek array)
+*/
+
+const dogsSorted = dogs.slice().sort((a, b) => a.recFood - b.recFood);
+console.log(dogsSorted);
