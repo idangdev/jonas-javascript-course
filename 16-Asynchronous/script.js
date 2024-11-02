fd78c9825d6f79e1c893c64bee8d2804b0cd6f69
idangdev <idang.dev@gmail.com>
2024-11-02 11:49:53 +0700

    259. The Event Loop in Practice

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -259,7 +259,7 @@ TEST COORDINATES 2: -33.933, 18.474
 
 GOOD LUCK 😀
 */
-
+/*
 const getJSONGeo = function (url, message) {
   return fetch(url).then(function (response) {
     console.log(response);
@@ -284,7 +284,6 @@ const whereAmI = function (lat, lng) {
       console.log(data);
       if (data.error) throw new Error(`Problem with geocoding`);
 
-      console.log(data);
       console.log(`You are in ${data.city}, ${data.country}`);
 
       return fetch(`https://restcountries.com/v2/name/${data.country}`);
@@ -305,13 +304,18 @@ const whereAmI = function (lat, lng) {
 whereAmI(52.508, 13.381);
 whereAmI(19.037, 72.873);
 whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
-whereAmI(-33.933, 18.474);
+*/
+
+///////////////////////////////////////
+// The Event Loop in Practice
+
+console.log('Test start');
+setTimeout(() => console.log('0 sec timer'), 0);
+Promise.resolve('Resolved promise 1').then(res => console.log(res));
+
+Promise.resolve('Resolved promise 2').then(res => {
+  for (let i = 0; i < 1000000000; i++) {}
+  console.log(res);
+});
+
+console.log('Test end');
