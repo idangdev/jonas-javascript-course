d6a3f041c20d84c4cf03a4c149f81ea41489dd15
idangdev <idang.dev@gmail.com>
2024-11-05 14:38:09 +0700

    266. Running Promises in Parallel

--- 16-Asynchronous/script.js
+++ 16-Asynchronous/script.js
@@ -28,6 +28,14 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 ///////////////////////////////////////
 // Our First AJAX Call: XMLHttpRequest
 
@@ -508,7 +516,7 @@ createImage('img/img-1.jpg')
 
 ///////////////////////////////////////
 // Consuming Promises with Async/Await
-
+/*
 const getPosition = function () {
   return new Promise(function (resolve, reject) {
     navigator.geolocation.getCurrentPosition(resolve, reject);
@@ -522,7 +530,8 @@ const getPosition = function () {
 const whereAmI = async function () {
   // Geolocation
   try {
-    const auth = '417365421947760475057x17005';
+    // const auth = '417365421947760475057x17005';
+    const auth = '121611352845353356139x22741';
 
     const pos = await getPosition();
     const { latitude: lat, longitude: lng } = pos.coords;
@@ -575,3 +584,29 @@ console.log('1: Will get location');
   }
   console.log('3: Finished getting location');
 })();
+*/
+
+///////////////////////////////////////
+// Running Promises in Parallel
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v2/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v2/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v2/name/${c3}`);
+
+    // console.log([data1.capital, data2.capital, data3.capital]);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v2/name/${c1}`),
+      getJSON(`https://restcountries.com/v2/name/${c2}`),
+      getJSON(`https://restcountries.com/v2/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+get3Countries('portugal', 'canada', 'tanzania');
