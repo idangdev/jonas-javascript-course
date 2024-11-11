fe9b1fdad32c1235b737baaad5f050d9f71a409f
idangdev <idang.dev@gmail.com>
2024-11-11 14:24:11 +0700

    275. The Module Pattern

--- 17-Modern-JS-Modules-Tooling/script.js
+++ 17-Modern-JS-Modules-Tooling/script.js
@@ -1,12 +1,12 @@
 ////////////////////////////////////////
 // Exporting and Importing in ES6 Modules
-
+/*
 // Importing module
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 // addToCart('bread', 5);
 // console.log(price, tq);
 
-console.log('Importing module');
+// console.log('Importing module');
 // console.log(shippingCost);
 
 // import * as ShoppingCart from './shoppingCart.js';
@@ -22,9 +22,14 @@ add('bread', 5);
 add('apples', 4);
 
 console.log(cart);
+*/
+
+import shoppingCart from './shoppingCart.js';
 
 ////////////////////////////////////////
 // Top-Level await (2022)
+
+/*
 
 // console.log('Start fetching');
 // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
@@ -47,3 +52,37 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+////////////////////////////////////////
+// The Module Pattern
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(
+      `${quantity} ${product} added to cart (shipping cost is ${shippingCost})`
+    );
+  };
+
+  const orderStock = function (product, quantity) {
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+shoppingCart2.addToCart('apple', 4);
+shoppingCart2.addToCart('pizza', 2);
+console.log(shoppingCart2);
+console.log(shoppingCart2.shippingCost);
