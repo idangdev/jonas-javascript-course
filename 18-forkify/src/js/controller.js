7f38cfac7a865764287ea5e9c2ed1624c9ec9307
idangdev <idang.dev@gmail.com>
2024-11-18 13:52:10 +0700

    291. Listening For load and hashchange Events

--- 18-forkify/src/js/controller.js
+++ 18-forkify/src/js/controller.js
@@ -31,11 +31,15 @@ const renderSpinner = function (parentEl) {
 
 const showRecipe = async function () {
   try {
+    const id = window.location.hash.slice(1);
+    console.log(id);
+
+    if (!id) return;
+
     // 1) Loading recipe
     renderSpinner(recipeContainer);
     const res = await fetch(
-      // 'https://forkify-api.herokuapp.com/api/v2/recipes/664c8f193e7aa067e94e863d'
-      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
 
     const data = await res.json();
@@ -163,4 +167,6 @@ const showRecipe = async function () {
   }
 };
 
-showRecipe();
+['hashchange', 'load'].forEach(ev => window.addEventListener(ev, showRecipe));
+// window.addEventListener('hashchange', showRecipe);
+// window.addEventListener('load', showRecipe);
